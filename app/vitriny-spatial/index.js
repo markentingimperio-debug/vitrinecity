6764aeb17b2ffd0afdc15ca7cad25df1a238bded
markentingimperio-debug <markentingimperio@gmail.com>
2026-09-07 15:06:59 -0300

    feat(spatial): expose spatial core v1

---
+++ app/vitriny-spatial/index.js
@@ -0,0 +1,7 @@
+export {createWorldRouter,parseSpatialPath,spatialPath,spatialRouteKey} from './world-router.js';
+export {chunkCoords,chunkId,desiredChunkSet,diffChunks,createChunkEngine} from './chunk-engine.js';
+export {RENDER_PROFILES,assessDevice,adaptProfile} from './device-profile.js';
+export {buildingTemplate,generateBuilding,generateBlock,spatialBuildingTemplates} from './procedural-city.js';
+export {CENTRAL_PLAZA_ID,centralPlazaLayout,centralPlazaPortals,spatialDistricts} from './central-plaza.js';
+
+export const VITRINY_SPATIAL_VERSION=1;
